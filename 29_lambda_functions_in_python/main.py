588eadae56b0b9bb9769aa8a4e2db389c090c13a
ySodias <ysodias@gmail.com>
2023-01-02 23:24:29 -0300

    first class

---
+++ 29_lambda_functions_in_python/main.py
@@ -0,0 +1,22 @@
+add = lambda x, y: x + y
+"""
+Funções lambda (funções sem nome) possuem 4 partes:
+
+1st -> palavra lambda
+2nd -> lista de parâmetros
+3rd -> : (cólon)
+4th -> valor de retorno
+"""
+
+print(add(5, 7))
+
+def double(x):
+    return x * 2
+
+sequence = [1, 3, 5, 9]
+doubled = [double(x) for x in sequence]
+d = list(map(lambda x: x * 2, sequence))
+#necessário converter map para list, pois map sempre retornará um objeto
+
+print(doubled)
+print(d)
